5fb9903d07dd99f0039913d552d906000d46c2c1
qweriikk <dashakots13@gmail.com>
2022-12-20 18:01:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,25 @@ const student = "Ершова Дарья Владимировна";
 
 document.getElementById("student").innerHTML = student;
 
-function getRandomIntInclusive(min, max) {
-    min = Match.ceil(min);
-    max = Match.floor(max);
-    return Match.floor(Match.random() * (max - min + 1)) + min;
-}
+const errorRate = 0.2;
 
-function throwRandomError(errorRate) {
-    if (getRandomIntInclusive(0, 20) <= errorRate) {
-        throw new Error("Ой, что-то сломалось");
-    }
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+ }
+  function throwRandomError(errorRate) {
+ 
+   if (getRandomIntInclusive(0, 1.0) <= errorRate) {
+      throw new Error('Ой, что-то сломалось');
+}
 }
 
 try {
-    throwRandomError(errorRate);
-
-    console.log("Всё в порядке, приложение работает в штатном режиме");
-
-} catch(err) {
-    
-    console.log("Произошла ошибка, пожалуйста перезагрузите страницу");
-
-    console.log(err);
-}
+  throwRandomError(errorRate) ;
+  console.log('Всё в порядке, приложение работает в штатном режиме');
+  
+  } catch (err) {
+  console.log('Произошла ошибка, пожалуйста перезагрузите страницу');
+  console.log(err);
+  }
